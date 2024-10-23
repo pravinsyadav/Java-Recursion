867f1aaa12b14f08d9c032c3e8a696dd16399f6e
pravinsyadav <pravinsy2003@gmail.com>
2024-10-23 22:54:13 +0530

    add recursion file

---
+++ Recursion1.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Recursion1 {
+    public static void printNumb(int n){   
+        if(n==0)         // base case condition
+        return;
+        System.out.println(n);         // print number
+        printNumb(n-1);                 // here fucntion calling itself
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        printNumb(n);
+    }
+}
