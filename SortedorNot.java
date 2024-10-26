689482a960422fda4ba74b4cdc21dfd948f30ef6
pravinsyadav <pravinsy2003@gmail.com>
2024-10-26 17:04:35 +0530

    sortedno.java

---
+++ SortedorNot.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class SortedorNot {
+    public static boolean isSorted(int arr[], int idx){
+        if(idx == arr.length-1){
+            return true;
+        }
+        if(arr[idx] < arr[idx+1]){
+            return isSorted(arr, idx+1);
+        }else{
+            return false;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+        int arr[] = new int[size]; 
+
+        for(int i = 0; i< size; i++){
+            arr[i] = sc.nextInt();
+        }
+        System.out.println(isSorted(arr, 0)); 
+    }
+}
